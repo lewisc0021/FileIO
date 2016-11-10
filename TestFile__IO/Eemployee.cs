45dfebaa7c09bd88dcb31f69f2a8e990748a7b96
Tommy Lewis <lewisc0021@gmail.com>
2016-11-10 11:20:02 -0500

    Initial Commit

---
+++ TestFile__IO/Eemployee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFile__IO
+{
+    class Employee
+    {
+        private string Name;
+        private float Salary;
+
+        #region Constructor
+        public Employee(string name, float salary)
+        {
+            Name1 = name;
+            Salary1 = salary;
+        }
+        #endregion
+
+        #region Properties
+        public string Name1
+        {
+            get
+            {
+                return Name;
+            }
+
+            set
+            {
+                Name = value;
+            }
+        }
+
+        public float Salary1
+        {
+            get
+            {
+                return Salary;
+            }
+
+            set
+            {
+                Salary = value;
+            }
+        }
+        #endregion
+
+    } }//END---------
---
+++ TestFile__IO/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFile__IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            List<Employee> EmployeeList = ReadDataFromFile();
+
+            ///////////////////////////////////////////////////
+            foreach (Employee Emp in EmployeeList)
+            {
+                Console.WriteLine($"Name : {Emp.Name1}, Salary : {Emp.Salary1}");
+            }
+
+            WriteDataToFile("Q","100000");
+
+        }//MAIN
+
+        private static void WriteDataToFile(string name, string salary)
+        {
+            StreamWriter sw = new StreamWriter("../../DataFile.txt", true);
+            sw.Write($"\n{name},{salary}");
+            sw.Close();
+        }
+
+        private static List<Employee> ReadDataFromFile()
+        {
+            List<Employee> EmployeeList = new List<Employee>();
+
+            string fileLocation = "../../DataFile.txt";
+
+            StreamReader reader = new StreamReader(fileLocation);
+
+            string Data = reader.ReadToEnd().Trim();
+
+            string[] Records = Data.Split('\n');
+
+            foreach (string record in Records)
+            {
+                string[] rc = record.Split(',');
+                EmployeeList.Add(new Employee(rc[0].Trim(), float.Parse(rc[1].Trim())));
+            }
+            reader.Close(); //must close resources before you try to use it again in another capacity
+            return EmployeeList;
+        }
+    }
+}
